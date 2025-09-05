098fd41d2b5cf180432c3399169f52a38c176a62
TaTia <tatyanakoryachkina@yandex.ru>
2025-09-06 00:52:15 +0300

    feat: did the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  let flag = true;
+  const openBrackets = [];
+  const closeBrackets = [];
+  const sameBrackets = bracketsConfig.flat();
+
+  sameBrackets.forEach((element, index) => {
+    if (index % 2 !== 0) closeBrackets.push(element);
+    else openBrackets.push(element);
+  });
+  [...str].forEach((element) => {
+    if (
+      !closeBrackets.includes(element) ||
+      (closeBrackets.includes(element) &&
+        openBrackets.includes(element) &&
+        stack[stack.length - 1] !== element)
+    ) {
+      stack.push(element);
+    } else {
+      const openBracketIndex = sameBrackets.lastIndexOf(element);
+      const prevBracket = sameBrackets[openBracketIndex - 1];
+      const lastSetElement = stack[stack.length - 1];
+      if (lastSetElement !== prevBracket) flag = false;
+      else stack.pop();
+    }
+  });
+  if (stack.length > 0) flag = false;
+  return flag;
 };
